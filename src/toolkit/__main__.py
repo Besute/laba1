c457f7f56a4adb43f9f196d7f6dd87043eca6fbc
Boris <boris200723@gmail.com>
2026-09-19 22:23:23 +0300

    Made parse logic

--- src/toolkit/__main__.py
+++ src/toolkit/__main__.py
@@ -2,19 +2,19 @@ import argparse
 from calculator import calculate
 
 def main():
-    # parser = argparse.ArgumentParser(
-    #     prog="toolkit",
-    #     description="This program calculates the result of a given expression or convert one measure to another"
-    # )
-    # subparsers = parser.add_subparsers(dest="command", required=True)
-    #
-    # calc_parser = subparsers.add_parser("calc", help="calculate expression")
-    # calc_parser.add_argument("value", type=str, help="expression")
-    #
-    # args = parser.parse_args()
-    # result = ""
-    # if args.command == "calc":
-    #     result = calculate(args.value)
+    parser = argparse.ArgumentParser(
+        prog="toolkit",
+        description="This program calculates the result of a given expression or convert one measure to another"
+    )
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    calc_parser = subparsers.add_parser("calc", help="calculate expression")
+    calc_parser.add_argument("value", type=str, help="expression")
+
+    args = parser.parse_args()
+    result = ""
+    if args.command == "calc":
+        result = calculate(args.value)
     result = calculate(input())
     print(result)
 
