688f00c2f41f719083a9c62939d84dda94564d76
Boris <boris200723@gmail.com>
2026-09-19 23:05:27 +0300

    Added logic of convertion (no debug)

---
+++ src/toolkit/converter.py
@@ -0,0 +1,58 @@
+LENGTH_REL_M = {
+    "km": 1000,
+    "m": 1,
+    "cm": 0.01,
+    "mm": 0.001
+}
+
+MASS_REL_G = {
+    "g": 1,
+    "kg": 1000
+}
+
+def faren_to_c(faren):
+    return (faren - 32) * 5/9
+
+def kel_to_c(k):
+    return k - 273.15
+
+def c_to_kel(c):
+    return c + 273.15
+
+def c_to_faren(c):
+    return c * 9/5 + 32
+
+def execute_length(val, from_, to_):
+    val_in_m = LENGTH_REL_M[from_] * val
+    val_ans = LENGTH_REL_M[to_] * val_in_m
+    return val_ans
+
+def execute_mass(val, from_, to_):
+    val_in_m = LENGTH_REL_M[from_] * val
+    val_ans = LENGTH_REL_M[to_] * val_in_m
+    return val_ans
+
+def execute_temper(val, from_, to_):
+    val_in_c = val
+    if from_ == "f":
+        val_in_c = faren_to_c(val)
+    elif from_ == "k":
+        val_in_c = kel_to_c(val)
+    val_ans = val_in_c
+    if to_ == "f":
+        val_ans = c_to_faren(val_ans)
+    elif to_ == "k":
+        val_ans = c_to_kel(val_ans)
+    return val_ans
+
+def evaluate_from(val, from_, to_):
+    if (from_ in "km;m;cm;mm"):
+        return execute_length(val, from_, to_)
+    elif (from_ in "g;kg"):
+        return execute_length(val, from_, to_)
+    elif (from_ in "c;f;k"):
+        return execute_temper(val, from_, to_)
+    raise SyntaxError(f"INCORRECT MEASURE: CAN'T CONVER FROM {from_} to {to_}")
+
+def convert(val, from_, to_):
+    return evaluate_from(val, from_, to_)
